5354436bc4dca7661c651242277e03c78969e738
jesusturpin <jesus.turpin@murciaeduca.es>
2025-04-03 11:45:07 +0200

    añadido fichero java

---
+++ src/Descuento.java
@@ -0,0 +1,31 @@
+public class Descuento {
+    public static double calculateDiscount(int productCount, double totalPrice, boolean isPremiumMember) {
+        double discount = 0;
+
+        if (productCount < 5) {
+            discount += 0;
+        } else if (productCount < 10) {
+            discount += 0.1;
+        } else {
+            discount += 0.15;
+        }
+
+        if (totalPrice > 500) {
+            discount += 0.1;
+        } else if (totalPrice > 200) {
+            discount += 0.05;
+        }
+
+        if (isPremiumMember) {
+            discount += 0.05;
+        }
+
+        double finalDiscount = discount * totalPrice;
+
+        if (finalDiscount > 0.3 * totalPrice) {
+            return 0.3 * totalPrice;
+        } else {
+            return finalDiscount;
+        }
+    }
+}
